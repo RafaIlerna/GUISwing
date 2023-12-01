afdf6c002e71ee54865d0064ae6f792a219d39f6
RafaIlerna <113125370+RafaIlerna@users.noreply.github.com>
2023-12-01 20:15:00 +0100

    Primero

---
+++ GUISwing/src/Main.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame(); //Crea un frame
+		frame.setTitle("JFrame title goes here"); //Titulo de frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+		frame.setResizable(false); //prevent frame from being resized
+		frame.setSize(420,420); // sets the x-dimensions, and Y - dimensions of frame
+		frame.setVisible(true); //make frame visible
+		
+		JPanel panel = new JPanel(); //Crea un panel
+		frame.add(panel); //Agrega el panel al frame
+		
+		ImageIcon image = new ImageIcon("logo.png"); //create an ImageIcon
+		JLabel label = new JLabel("<html><img src=\"https://picsum.photos/200\"></html>");
+		panel.add(label); //Crea un JLabel con el ImageIcon
+		
+		frame.setIconImage(image.getImage());// change icon of frame
+		frame.getContentPane().setBackground(new Color(0,255,0)); //Change color of background
+		
+		
+		
+	}
+
+}
